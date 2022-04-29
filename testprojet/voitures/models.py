dcb3d2dd39f85583a0bf493c099ff6e832632101
nicolas kayser <nicolas.kayser@uha.fr>
2022-04-29 11:33:49 +0200

    commit

--- testprojet/voitures/models.py
+++ testprojet/voitures/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Automobile(models.Model):
     manufacture = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    date_commercialisation = models.CharField(max_length=100)
+    date_commercialisation = models.DateField(blank = True, null = False)
     motorisation = models.CharField(max_length=100)
     transmission = models.CharField(max_length=100)
     boite_de_vitesse = models.CharField(max_length=100)
